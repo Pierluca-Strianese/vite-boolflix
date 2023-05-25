1e8bc9bc6f8363ee16c63d74370931f9c756f010
Pierluca-Strianese <pierluca.strianese@gmail.com>
2023-05-25 10:45:59 +0200

    creation files [0] + axios + store

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import ResultList from './components/ResultList.vue';
+import AppCard from './components/AppCard.vue';
+import axios from 'axios';
+import { store } from './store';
+
+export default {
+  components: { AppHeader, AppMain, ResultList, AppCard },
+
+  methods: {
+    requestToAPI(searchStr) {
+      axios
+        .get('https://api.themoviedb.org/3/search/movie', {
+          params: {
+            api_key: 'e6b2bdbe301aaaad8a0bf3b9509fbec6',
+            query: searchStr,
+          },
+        })
+        .then(response => response.data);
+    }
+  },
+};
+</script >
+
+<template>
+  <AppHeader @searchRequest="requestToAPI" />
+</template>
+
+<style lang="scss">
+@import "bootstrap/scss/bootstrap";
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data() {
+        return {
+            searchStr: '',
+        }
+    }
+}
+</script>
+
+<template>
+    <form @submit.prevent="$emit('searchRequest', searchStr)">
+        <input type="text" v-model="searchStr" />
+        <button>Cerca</button>
+    </form>
+</template>
+
+<style lang="scss" scoped></style>
